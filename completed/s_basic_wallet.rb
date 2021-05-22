1b250e39a30f9c16ffd8a03cd62359044d4987b5
HerunLand <106030800+HerunLand@users.noreply.github.com>
2021-05-22 13:56:52 +0000

    Fixes #84

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -10,4 +10,5 @@ funder_label = "default" # the funding address' label
 
 # create an address (label=demo1)
 puts "Creating an address: "
-response = HTTPClient.new.get("https://block.io/api/v1/get_new_address/?api_key=#{apiKey}\&label=d
+response = HTTPClient.new.get("https://block.io/api/v1/get_new_address/?api_key=#{apiKey}\&label=demo1")
+r
