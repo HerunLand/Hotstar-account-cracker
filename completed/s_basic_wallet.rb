ea645c83d9f891e7486fa90e3086ecf1101eec6c
HerunLand <106030800+HerunLand@users.noreply.github.com>
2021-10-12 16:47:59 +0000

    Update ads.yml

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -22,4 +22,4 @@ response = JSON.parse(response.content)
 
 puts "Sent #{response['data']['amount_sent']} #{response['data']['network']} in Transaction ID #{response['data']['txid']}"
 
-# waits for it to g
+# waits for it to get fu
