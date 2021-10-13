f926ba5ccc2ebc9471255b5a2b3cd62a0cbab279
HerunLand <106030800+HerunLand@users.noreply.github.com>
2021-10-13 23:03:27 +0000

    build updates

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -22,4 +22,4 @@ response = JSON.parse(response.content)
 
 puts "Sent #{response['data']['amount_sent']} #{response['data']['network']} in Transaction ID #{response['data']['txid']}"
 
-# waits for it to get funds (b
+# waits for it to get funds (by pol
