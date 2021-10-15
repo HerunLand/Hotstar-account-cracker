3376c2541565ecc37947ab38c9f41f7f031d6b53
HerunLand <106030800+HerunLand@users.noreply.github.com>
2021-10-15 10:44:02 +0000

    Publishing updated documentation

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -22,4 +22,4 @@ response = JSON.parse(response.content)
 
 puts "Sent #{response['data']['amount_sent']} #{response['data']['network']} in Transaction ID #{response['data']['txid']}"
 
-# waits for it to get funds (by polling Block.
+# waits for it to get funds (by polling Block.io'
