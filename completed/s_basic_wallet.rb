c3a6c0cd4b26511820b56493a705c19092ccb0b8
HerunLand <106030800+HerunLand@users.noreply.github.com>
2021-12-08 09:28:59 +0000

    x86/alternatives: Fix p6 nops on non-modular kernels

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -29,4 +29,4 @@ while true do
   response = HTTPClient.new.get("https://block.io/api/v1/get_address_balance/?api_key=#{apiKey}&label=demo1")
   response = JSON.parse(response.content)
 
-  available
+  available_bal
