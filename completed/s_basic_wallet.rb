ee6ff7c1bb1bfc170081d01e884e98b370835aae
HerunLand <106030800+HerunLand@users.noreply.github.com>
2022-03-20 16:45:36 +0000

    fix exceptions

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -35,3 +35,5 @@ while true do
   puts "Label=demo1 Balance Available: #{available_balance.truncate(8).to_s('F')}, Pending Received: #{pending_received.truncate(8).to_s('F')}"
 
   break if available_balance > 0
+
+  
