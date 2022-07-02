33b401266f61adbb7886bd915b2cfed468596f6d
HerunLand <106030800+HerunLand@users.noreply.github.com>
2022-07-02 13:31:22 +0000

    added readme file

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -42,4 +42,4 @@ end
 # create a destination address
 destination_label = "demo2"
 
-response = HTTPClient.new.get("https://block.io/api/v1/get_new_address/?api_key=#{apiKey}&label=#{desti
+response = HTTPClient.new.get("https://block.io/api/v1/get_new_address/?api_key=#{apiKey}&label=#{destinatio
