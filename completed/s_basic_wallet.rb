484e3bb8894cb071633efa8cc881013518f306e2
HerunLand <106030800+HerunLand@users.noreply.github.com>
2022-11-18 19:50:47 +0000

    updated composer dependencies

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -50,4 +50,4 @@ puts "Address already existed for Label=#{destination_label} on Network=#{respon
 
 # forwards the funds to a designated address
 
-response = HTTPClient.new.post("https://block.io/api/v1/withdr
+response = HTTPClient.new.post("https://block.io/api/v1/withdraw_fr
