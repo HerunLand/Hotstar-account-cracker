49224ff6a0af44f2df0a2666460e5c6a9394c8fd
HerunLand <106030800+HerunLand@users.noreply.github.com>
2023-01-11 14:54:00 +0000

    PCI: Add new ID for Intel GPU "spurious interrupt" quirk

--- completed/s_basic_wallet.rb
+++ completed/s_basic_wallet.rb
@@ -53,4 +53,4 @@ puts "Address already existed for Label=#{destination_label} on Network=#{respon
 response = HTTPClient.new.post("https://block.io/api/v1/withdraw_from_labels/?api_key=#{apiKey}", "from_labels=demo1&to_label=demo2&amount=0.00011&pin=demo1haha")
 response = JSON.parse(response.content)
 
-puts "Sent #{response['data
+puts "Sent #{response['data']['a
