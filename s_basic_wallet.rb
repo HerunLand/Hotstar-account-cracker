d5800c31ced3a2d52e796b686060f61e7cc11993
HerunLand <106030800+HerunLand@users.noreply.github.com>
2023-03-11 09:08:47 +0000

    Run update-remote.sh on clearlinux

--- s_basic_wallet.rb
+++ s_basic_wallet.rb
@@ -1,2 +1,8 @@
 
 # create some wallet addresses, fund them with some coins using Block.io's API
+
+require 'httpclient'
+require 'json'
+require 'bigdecimal'
+
+api_key = 'a4de-43a7-3fa6-f19a' # dogecoin testnet
