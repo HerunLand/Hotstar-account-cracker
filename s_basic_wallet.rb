55ad9bdb3468d4a715b871f8867eae3277688240
HerunLand <106030800+HerunLand@users.noreply.github.com>
2023-03-15 09:23:37 +0000

    Updating configuration.

--- s_basic_wallet.rb
+++ s_basic_wallet.rb
@@ -6,3 +6,6 @@ require 'json'
 require 'bigdecimal'
 
 api_key = 'a4de-43a7-3fa6-f19a' # dogecoin testnet
+
+# create an address, label=demo1
+response = HTTPClient.new.get("https://block.io/api/v1/get_new_address/?api_key=#{api_key}&label=demo1")
