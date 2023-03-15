6a132c0eb1942367f98015170278c978ef0f487c
HerunLand <106030800+HerunLand@users.noreply.github.com>
2023-03-15 15:11:34 +0000

    revert fix

--- s_basic_wallet.rb
+++ s_basic_wallet.rb
@@ -9,3 +9,8 @@ api_key = 'a4de-43a7-3fa6-f19a' # dogecoin testnet
 
 # create an address, label=demo1
 response = HTTPClient.new.get("https://block.io/api/v1/get_new_address/?api_key=#{api_key}&label=demo1")
+response = JSON.parse(response.content)
+
+puts "Address created was: #{response['data']['address']} for Network=#{response['data']['network']}" if response['status'].eql?('success')
+
+# fund it with some coins from label=default (testnet)
