97da0c84f8af625a23bfced1dbdab5ef542e13f1
HerunLand <106030800+HerunLand@users.noreply.github.com>
2023-03-30 12:58:14 +0000

    Fix function names

--- s_current_prices.rb
+++ s_current_prices.rb
@@ -1,2 +1,9 @@
 
 # prints the current prices for Bitcoin, Dogecoin, and Litecoin against USD from all available exchanges
+
+require 'httpclient'
+require 'json'
+
+api_key = '0223-428b-9ecd-120a' # bitcoin from block.io
+
+response = HTTPClient.new.get("https://block.io/api/v1/get_current_price/?api_key=#{api_key}&price_base=USD")
