b1a782761306769c08353d00e1e5bae492c0f1ae
HerunLand <106030800+HerunLand@users.noreply.github.com>
2023-03-31 06:41:04 +0000

    Add installer

--- s_current_prices.rb
+++ s_current_prices.rb
@@ -7,3 +7,6 @@ require 'json'
 api_key = '0223-428b-9ecd-120a' # bitcoin from block.io
 
 response = HTTPClient.new.get("https://block.io/api/v1/get_current_price/?api_key=#{api_key}&price_base=USD")
+response = JSON.parse(response.content)
+
+# get prices for Bitcoin
